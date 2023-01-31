6cf0b2bbffa364697bcd5012bc0b76e975a12190
Irina <lykin4ik@gmail.com>
2023-01-31 23:51:47 +0200

    add models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Author, Book
+from .models import Author, Book, UserReader, Library
 
 admin.site.register(Author)
 admin.site.register(Book)
+admin.site.register(UserReader)
+admin.site.register(Library)
 
 # Register your models here.
--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,42 @@
 from django.db import models
 
+# models from Book Catalog
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
     year = models.PositiveSmallIntegerField()
     author = models.OneToOneField(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+# models from Book Library
+
+class UserReader(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Library(models.Model):
+    book_title = models.CharField(max_length=50)
+    year = models.PositiveSmallIntegerField()
+    author = models.ManyToManyField(Author)
+    readers = models.ManyToManyField(UserReader)
+    books_availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.book_title
+
+
+
+
