4a36d414ea682dced9b1df7c1bf25ae4136050de
Irina <lykin4ik@gmail.com>
2023-01-31 20:14:30 +0200

    First commit

--- homework1/settings.py
+++ homework1/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'homework1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_homework3',
+        'USER': 'user1',
+        'PASSWORD': '28122012',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
